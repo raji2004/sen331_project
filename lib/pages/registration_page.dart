d718851bec53c81d9b8ef1c875dfa7ab1134d5f4
raji <kingraj1344@gmail.com>
2024-01-16 13:56:52 +0100

    first page

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../widget/widget.dart';
+import '../utils/utils.dart';
 
 class RegistrationPage extends StatelessWidget {
   const RegistrationPage({
@@ -8,12 +10,19 @@ class RegistrationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(image: AssetImage('assets/icons/logo.png')),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage('assets/icons/logo.png'),
+              width: 100,
+            ),
+            MyText("Welcome to the Elowen",fontSize: 32, fontWeight: FontWeight.w400,),
+            SizedBox(height: 10),
+            MyText("Please log in or sign up to continue shopping",fontSize: 16, fontWeight: FontWeight.w400,color: Secondary.grey, ),
+          ],
+        ),
       ),
     );
   }
